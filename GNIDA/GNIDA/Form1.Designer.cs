7def8613459062b96e9572bb8e2029e937ffbc48
Shooshpanchik <Shooshpanchik@inbox.ru>
2014-05-24 21:36:39 +0600

    Variables proceded. RC1

--- GNIDA/GNIDA/Form1.Designer.cs
+++ GNIDA/GNIDA/Form1.Designer.cs
@@ -385,6 +385,7 @@
             this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.fastColoredTextBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
             this.fastColoredTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fastColoredTextBox1.Font = new System.Drawing.Font("Courier New", 9.75F);
             this.fastColoredTextBox1.IsReplaceMode = false;
             this.fastColoredTextBox1.Language = FastColoredTextBoxNS.Language.CSharp;
             this.fastColoredTextBox1.LeftBracket = '(';
--- GNIDA/GNIDA/GNIDA.cs
+++ GNIDA/GNIDA/GNIDA.cs
@@ -9,6 +9,60 @@ using System.ComponentModel;
 
 namespace GNIDA
 {
+    /*
+    public static class stringExt
+    {
+        static object obj;
+        public static void str(this string ss)
+        {
+
+            //obj = ss;
+        }
+    }*/
+    public class VarDictionary : Dictionary<ulong, TVar>
+    {
+        public GNIDA Parent;
+        public void AddVar(TVar value)
+        {
+            if (!this.ContainsKey(value.Addr))
+            {
+                this.Add(value.Addr, value);
+                Parent.RaiseVarFuncEvent(this, value);
+            }
+        }
+    }
+
+    public class TVar
+    {
+        public ulong Addr;
+        public string FName;
+        public uint type;
+        public string ToStr()
+        {
+            switch(type)
+            {
+                case 0: return "void " + FName;
+                case 1: return "byte " + FName;
+                case 2: return "word " + FName;
+                case 4: return "dword " + FName;
+                default : return "void " + FName;
+            }
+        }
+        public TVar(ulong addr, string Name = "", uint Type = 0)
+        {
+            Addr = addr;
+            type = Type;
+            if (Name != "") FName = Name;
+            else
+                switch (Type)
+                {
+                    case 1: FName = "byte_" + Addr.ToString("X8"); break;
+                    case 2: FName = "word_" + Addr.ToString("X8"); break;
+                    case 4: FName = "dword_" + Addr.ToString("X8"); break;
+                    default: FName = "unk_" + Addr.ToString("X8"); break;
+                }
+        }
+    }
     public class MyDictionary : Dictionary<ulong, TFunc>
     {
         public GNIDA Parent;
@@ -24,33 +78,55 @@ namespace GNIDA
             }
         }
     }
+    public static class OperandExtensions
+    {
+        public static string ToString(this Operand Op)
+        {
+            return "0x" + Op.ToString(false);
+        }
+    }
     public static class X86Extensions
     {
-        public static string ToCmmString(this x86Instruction inst, GNIDA Parent, Dictionary<ulong, TFunc> NewSubs)
+        private static string AddProc(Offset x, MyDictionary ProcList, Dictionary<ulong, TFunc> NewSubs)
         {
-            if (inst.OpCode.OpCodeBytes[0] == 0x74) return "$JZ Loc_" + inst.Operand1.ToString(true);
-            if (inst.OpCode.OpCodeBytes[0] == 0x75) return "$JNZ Loc_" + inst.Operand1.ToString(true);
-            if (inst.OpCode.OpCodeBytes[0] == 0xEB) return "$JMP Loc_" + inst.Operand1.ToString(true);
-            if (inst.OpCode.OpCodeBytes[0] == 0xE8)
-                if (inst.Operand1.ValueType == OperandType.Normal)
-                {
-                    Offset x = (Offset)inst.Operand1.Value;
-                    if (Parent.FullProcList.ContainsKey(x.Va)) return Parent.FullProcList[x.Va].FName + "();";
-                    TFunc tmpfunc = new TFunc(x.Va, 1);
-                    if (!NewSubs.ContainsKey(x.Va)) NewSubs.Add(x.Va, tmpfunc);
-                    return "Sub_" + x.Va.ToString("X") + "()";
-                }
-            if (inst.OpCode.OpCodeBytes[0] == 0xFF)
-                if (inst.OpCode.OpCodeBytes[1] == 0x15)
-                    if (inst.Operand1.ValueType == OperandType.DwordPointer)
+            if (ProcList.ContainsKey(x.Va)) return ProcList[x.Va].FName + "();";
+            TFunc tmpfunc = new TFunc(x.Va, 1);
+            if (!NewSubs.ContainsKey(x.Va)) NewSubs.Add(x.Va, tmpfunc);
+            return "Sub_" + x.Va.ToString("X8") + "();";
+        }
+        public static string ToCmmString(this x86Instruction inst, MyDictionary ProcList, VarDictionary VarDict, Dictionary<ulong, TFunc> NewSubs)
+        {
+            switch(inst.OpCode.OpCodeBytes[0])
+            {
+                case 0x74: return "$JZ Loc_" + inst.Operand1.ToString(true);
+                case 0xA3://mov somevar, EAX
                     {
-                        Offset x = (Offset)inst.Operand1.Value;
-                        //Parent.FullProcList
-                        if (Parent.FullProcList.ContainsKey(x.Va)) return Parent.FullProcList[x.Va].FName + "();";
-                        TFunc tmpfunc = new TFunc(x.Va, 1);
-                        if (!NewSubs.ContainsKey(x.Va)) NewSubs.Add(x.Va, tmpfunc);
-                        return "Sub_" + x.Va.ToString("X") + "()";
+                        TVar Var1 = new TVar(((Offset)inst.Operand1.Value).Va, "", 4);
+                        if (!VarDict.ContainsKey(((Offset)inst.Operand1.Value).Va))
+                        {
+                            VarDict.AddVar(Var1);
+                        };
+                        return "$MOV " + VarDict[((Offset)inst.Operand1.Value).Va].FName + ", EAX";
                     }
+                case 0x75: return "$JNZ Loc_" + inst.Operand1.ToString(true);
+                case 0xEB: return "$JMP Loc_" + inst.Operand1.ToString(true);
+                case 0xC3: return "$RET";
+                case 0xE8:
+                    {
+                        if (inst.Operand1.ValueType == OperandType.Normal)
+                        {
+                            return AddProc((Offset)inst.Operand1.Value, ProcList, NewSubs);
+                        }
+                    }break;
+                case 0xFF:
+                    {
+                        if (inst.OpCode.OpCodeBytes[1] == 0x15)
+                        if (inst.Operand1.ValueType == OperandType.DwordPointer)
+                        {
+                            return AddProc((Offset)inst.Operand1.Value, ProcList, NewSubs);
+                        }
+                    }break;
+            }
             string str = inst.ToAsmString();
             str = str.Replace(" PTR ", "");
             return '$'+str;
@@ -75,15 +151,17 @@ namespace GNIDA
         }
     }
     public class GNIDA
-    {
+    {        
         public static Win32Assembly assembly;
         public BackgroundWorker bw = new BackgroundWorker();
         public MyDictionary FullProcList = new MyDictionary();
         public MyDictionary ToDisasmFuncList = new MyDictionary();
         public MyDictionary DisasmedFuncList = new MyDictionary();
+        public VarDictionary VarDict = new VarDictionary();
         public GNIDA()
         {
             FullProcList.Parent = this;
+            VarDict.Parent = this;
         }
         public void StopWork()
         {
@@ -115,14 +193,14 @@ namespace GNIDA
             {
                 string tmp = "";
                 if (Label != "") tmp = "/*" + (Inst.Offset.Rva + assembly.NTHeader.OptionalHeader.ImageBase).ToString("X8") + "*/ " + Label + ":\n";
-                tmp += "/*" + (Inst.Offset.Rva + assembly.NTHeader.OptionalHeader.ImageBase).ToString("X8") + "*/  " + Inst.ToCmmString(Parent, NewSubs);
+                tmp += "/*" + (Inst.Offset.Rva + assembly.NTHeader.OptionalHeader.ImageBase).ToString("X8") + "*/  " + Inst.ToCmmString(Parent.FullProcList, Parent.VarDict, NewSubs);
                 if (Comment != "") tmp += "// " + Comment;
                 tmp += "\n";
                 if (SubComment != "") tmp += "/*" + (Inst.Offset.Rva + assembly.NTHeader.OptionalHeader.ImageBase).ToString("X8") + "*/ // " + SubComment + "\n";
                 return tmp;
             }
         }
-        public List<Stroka> DisasmFunc(ulong addr)
+        public List<Stroka> DisasmFunc(ulong addr, MyDictionary ProcList)
         {
             List<Stroka> lst = new List<Stroka>();
             List<ulong> Tasks = new List<ulong>();
@@ -130,32 +208,50 @@ namespace GNIDA
             Tasks.Add(addr);
             for (uint i = 0; Tasks.Count > 0; i++)
             {
-                //to-do 
                 assembly.Disassembler.CurrentOffset = (uint)Tasks[0];
                 Tasks.Remove(Tasks[0]);
                 x86Instruction instruction = assembly.Disassembler.DisassembleNextInstruction();
                 lst.Add(new Stroka(this, instruction));
-                if (instruction.OpCode.OpCodeBytes[0] == 0xC3) continue;//retn
-                if (instruction.OpCode.OpCodeBytes[0] == 0xC2) continue;//retn XX;
-                if (instruction.OpCode.OpCodeBytes[0] == 0xEB) continue;//jmp;
-                if (instruction.OpCode.OpCodeBytes[0] == 0x74)// je/jz;
+                switch (instruction.OpCode.OpCodeBytes[0])
                 {
-                    int val = instruction.OperandBytes[0] + (int)instruction.Offset.FileOffset + instruction.Size;
-                    if (!LabelList.Contains(val))
-                    {
-                        LabelList.Add(val);
-                        Tasks.Add((uint)val);
-                    }
+                    case 0x74://Jxx
+                        {
+                            int val = instruction.OperandBytes[0] + (int)instruction.Offset.FileOffset + instruction.Size;
+                            if (!LabelList.Contains(val))
+                            {
+                                LabelList.Add(val);
+                                Tasks.Add((uint)val);//Add jmp adress to disasm tasks
+                            }
+                        }break;
+                    case 0xC2://retn XX;
+                    case 0xC3://retn
+                        continue;// Don't disasm after it
+                    case 0xEB://jmp;
+                        int val1 = instruction.OperandBytes[0] + (int)instruction.Offset.FileOffset + instruction.Size;
+                        if (!LabelList.Contains(val1))
+                        {
+                            LabelList.Add(val1);
+                            Tasks.Add((uint)val1);//Add jmp adress to disasm tasks
+                        }
+                        continue;// Don't disasm after it
+                    case 0xFF:
+                        Console.WriteLine("--");
+                        Console.WriteLine(instruction.OperandBytes[0].ToString("X2"));
+                        Console.WriteLine(instruction.OperandBytes[1].ToString("X2"));
+                        if (instruction.OpCode.OpCodeBytes[1] == 0x15)//Call
+                            {
+                                ulong a = ((Offset)instruction.Operand1.Value).Va;
+                                if(ProcList.ContainsKey(a))
+                                    if(ProcList[a].FName.Contains("ExitProcess"))continue;
+                            }
+                            break;
                 }
                 Tasks.Add(instruction.Offset.FileOffset + (uint)instruction.Size);
             }
             foreach (uint Addr in LabelList)
             {
                 Stroka result = lst.Find(
-                    delegate(Stroka sstr)
-                    {
-                        return sstr.addr == Addr;
-                    }
+                     delegate(Stroka sstr){return sstr.addr == Addr;}
                     );
                 if (result != null)
                 {
@@ -230,7 +326,7 @@ namespace GNIDA
                     break;
                 }
                 RaiseAddStrEvent("/*" + dct.Key.ToString("X8") + "*/ void " + dct.Value.FName + "(){\n");
-                List<Stroka> tmp = DisasmFunc(RVA2FO(dct.Key));
+                List<Stroka> tmp = DisasmFunc(RVA2FO(dct.Key), FullProcList);
                 foreach(Stroka t in tmp)
                 {
                     RaiseAddStrEvent(t.ToCmmString(NewSubs));
@@ -239,15 +335,19 @@ namespace GNIDA
                 RaiseAddStrEvent("};//" + dct.Value.FName + "\n");
             }
             ToDisasmFuncList.Clear();
-            /*
-            */
         }
         public delegate void LogEvent(object sender, string LogStr);
+        public delegate void VarEvent(object sender, TVar Var);
         public delegate void AddFuncEvent(object sender, TFunc Func);
         public delegate void AddStrEvent(string Str);
         public event LogEvent OnLogEvent;
+        public event VarEvent OnVarEvent;
         public event AddFuncEvent OnAddFunc;
         public event AddStrEvent OnAddStr;
+        public void RaiseVarFuncEvent(object sender, TVar Var)
+        {
+            if (OnVarEvent != null) OnVarEvent(sender, Var);
+        }
         private void RaiseLogEvent(object sender, string LogStr)
         {
             if (OnLogEvent != null) OnLogEvent(sender, LogStr);
